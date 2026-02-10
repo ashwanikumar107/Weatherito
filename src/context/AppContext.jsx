c6b18cca44fff10a9b2f40976cf1b5d3a29604b5
Ashwani Kumar <149175936+ashwanikumar107@users.noreply.github.com>
2026-02-10 23:05:34 +0530

    Update AppContext.jsx

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -9,8 +9,8 @@ import {
 const AppContext = createContext();
 
 const API_KEY = "ac97b4a45bf9f7f94d8d960d16fc3a36";
-const DEFAULT_LATITUDE = 30.0626;
-const DEFAULT_LONGITUDE = 31.2497;
+const DEFAULT_LATITUDE = 28.6315;
+const DEFAULT_LONGITUDE = 77.2167;
 
 async function fetchData(url, setter) {
   try {
